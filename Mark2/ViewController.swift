da9deab4ee78bc5582a1ce5c1e7cf9401a5b691d
Justin Lindberg <jlindber@bulldogs.aamu.edu>
2019-07-14 17:51:42 -0400

    created i go half with you bro

--- Mark2/ViewController.swift
+++ Mark2/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let iGoHalfWithYouBro = "hell nawl cuz"
         // Do any additional setup after loading the view.
     }
 
